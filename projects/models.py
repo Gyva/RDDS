a635252258840d7f7e0f20192a616ff91b69dcdf
UMWERE Christian <umwerechr97@gmail.com>
2024-08-21 13:42:47 +0200

    Implemented project model feature

---
+++ projects/models.py
@@ -0,0 +1,100 @@
+import os
+import uuid
+from django.db import models
+from django.core.validators import FileExtensionValidator
+
+# Function to generate a unique profile picture filename
+def unique_image_path(instance, filename):
+    ext = filename.split('.')[-1]
+    unique_filename = f"{uuid.uuid4().hex}.{ext}"
+    if isinstance(instance, Supervisor):
+        return os.path.join('static/supervisor/', unique_filename)
+    elif isinstance(instance, Student):
+        return os.path.join('static/student/', unique_filename)
+
+# Department Model
+class Department(models.Model):
+    dpt_id = models.AutoField(primary_key=True)
+    dpt_name = models.CharField(max_length=255)
+    
+    def __str__(self):
+        return self.dpt_name
+
+# Supervisor Model
+class Supervisor(models.Model):
+    ROLE_CHOICES = (
+        ('HoD', 'Head of Department'),
+        ('Lecturer', 'Lecturer'),
+    )
+    
+    sup_id = models.AutoField(primary_key=True)
+    reg_num = models.CharField(max_length=8, unique=True, editable=False)
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    profile_pic = models.ImageField(upload_to=unique_image_path, validators=[FileExtensionValidator(['jpg', 'jpeg', 'png'])])
+    specialization = models.CharField(max_length=255)
+    dpt_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    role = models.CharField(max_length=20, choices=ROLE_CHOICES)
+    
+    def save(self, *args, **kwargs):
+        if not self.reg_num:
+            last_sup = Supervisor.objects.all().order_by('sup_id').last()
+            if last_sup:
+                sup_num = int(last_sup.reg_num[3:]) + 1
+            else:
+                sup_num = 0
+            self.reg_num = f"sup{sup_num:05d}"
+        super(Supervisor, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+# Faculty Model
+class Faculty(models.Model):
+    f_id = models.AutoField(primary_key=True)
+    f_name = models.CharField(max_length=255)
+    dpt_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.f_name
+
+# Level Model
+class Level(models.Model):
+    l_id = models.AutoField(primary_key=True)
+    l_name = models.CharField(max_length=255)
+    dpt_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return self.l_name
+
+# Student Model
+class Student(models.Model):
+    st_id = models.AutoField(primary_key=True)
+    reg_no = models.CharField(max_length=10, unique=True, editable=False)
+    fname = models.CharField(max_length=100)
+    lname = models.CharField(max_length=100)
+    dob = models.DateField()
+    email = models.EmailField(unique=True)
+    phone = models.CharField(max_length=15)
+    dpt_id = models.ForeignKey(Department, on_delete=models.CASCADE)
+    f_id = models.ForeignKey(Faculty, on_delete=models.CASCADE)
+    l_id = models.ForeignKey(Level, on_delete=models.CASCADE)
+    profile_pic = models.ImageField(upload_to=unique_image_path, validators=[FileExtensionValidator(['jpg', 'jpeg', 'png'])])
+    
+    def save(self, *args, **kwargs):
+        if not self.reg_no:
+            last_student = Student.objects.all().order_by('st_id').last()
+            if last_student:
+                num_part = int(last_student.reg_no[2:7]) + 1
+            else:
+                num_part = 0
+            self.reg_no = f"24rp{num_part:05d}"
+        super(Student, self).save(*args, **kwargs)
+    
+    def __str__(self):
+        return f'{self.fname} {self.lname}'
+
+
+
--- rdds/settings.py
+++ rdds/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -37,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'projects'
 ]
 
 MIDDLEWARE = [
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'rdds.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'rdds',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
@@ -116,6 +122,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
