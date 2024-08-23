8316a267db91a890c6dc4d0706e98fb835465d2f
UMWERE Christian <umwerechr97@gmail.com>
2024-08-23 16:59:33 +0200

    Enhance CRUD functionality in views with context handling

--- projects/views.py
+++ projects/views.py
@@ -1,3 +1,49 @@
-from django.shortcuts import render
+from rest_framework import viewsets, serializers
+from .models import Department, Supervisor, Faculty, Level, Student
+from .serializers import DepartmentSerializer, SupervisorSerializer, FacultySerializer, LevelSerializer, StudentSerializer
 
-# Create your views here.
+class DepartmentViewSet(viewsets.ModelViewSet):
+    queryset = Department.objects.all()
+    serializer_class = DepartmentSerializer
+
+class SupervisorViewSet(viewsets.ModelViewSet):
+    queryset = Supervisor.objects.all()
+    serializer_class = SupervisorSerializer
+
+    def perform_create(self, serializer):
+        department = serializer.validated_data['dpt_id']
+        role = serializer.validated_data['role']
+
+        if role == 'HoD':
+            if Supervisor.objects.filter(dpt_id=department, role='HoD').exists():
+                raise serializers.ValidationError("A department can only have one HoD.")
+        
+        serializer.save()
+
+class FacultyViewSet(viewsets.ModelViewSet):
+    queryset = Faculty.objects.all()
+    serializer_class = FacultySerializer
+
+class LevelViewSet(viewsets.ModelViewSet):
+    queryset = Level.objects.all()
+    serializer_class = LevelSerializer
+
+    def get_serializer_context(self):
+        context = super().get_serializer_context()
+        context['request'] = self.request
+        return context
+
+
+class StudentViewSet(viewsets.ModelViewSet):
+    queryset = Student.objects.all()
+    serializer_class = StudentSerializer
+
+    def get_serializer_context(self):
+        # Provide the department ID and faculty ID in the context for the serializer
+        context = super().get_serializer_context()
+        request = self.request
+
+        # Extract `dpt_id` and `f_id` from the request data
+        context['dpt_id'] = request.data.get('dpt_id') if request.data else None
+        context['f_id'] = request.data.get('f_id') if request.data else None
+        return context
