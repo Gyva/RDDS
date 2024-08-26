c8a56f43a3990eaf6fb4a1125c4664fc0ddbb921
KWIZERA Ferdinand <realkwizera1@gmail.com>
2024-08-26 03:27:13 -0700

    Login responsiveness

--- projects/templates/projects/src/App.js
+++ projects/templates/projects/src/App.js
@@ -24,26 +24,30 @@ const App = () => {
 
     return (
         <Router>
-            <div className={`app-container ${isSidebarVisible ? 'sidebar-visible' : 'sidebar-hidden'}`}>
-                {isAuthenticated && <Sidebar isVisible={isSidebarVisible} />}
-                <div className="content-wrapper">
-                    {isAuthenticated && <Navbar toggleSidebar={toggleSidebar} isVisible={isSidebarVisible} />}
-                    <div className="main-content">
-                        <Routes>
-                            <Route path="/login" element={<Login onLogin={handleLogin} />} />
-                            {/* Protect routes by checking isAuthenticated */}
-                            {isAuthenticated ? (
-                                <>
-                                    <Route path="/register" element={<StudentRegistrationForm />} />
-                                    <Route path="*" element={<Navigate to="/register" />} />
-                                </>
-                            ) : (
-                                <Route path="*" element={<Navigate to="/login" />} />
-                            )}
-                        </Routes>
-                    </div>
-                </div>
-            </div>
+            <Routes>
+                <Route path="/login" element={<Login onLogin={handleLogin} />} />
+                <Route 
+                    path="*" 
+                    element={
+                        isAuthenticated ? (
+                            <div className={`app-container ${isSidebarVisible ? 'sidebar-visible' : 'sidebar-hidden'}`}>
+                                {isAuthenticated && <Sidebar isVisible={isSidebarVisible} />}
+                                <div className="content-wrapper">
+                                    {isAuthenticated && <Navbar toggleSidebar={toggleSidebar} isVisible={isSidebarVisible} />}
+                                    <div className="main-content">
+                                        <Routes>
+                                            <Route path="/register" element={<StudentRegistrationForm />} />
+                                            <Route path="*" element={<Navigate to="/register" />} />
+                                        </Routes>
+                                    </div>
+                                </div>
+                            </div>
+                        ) : (
+                            <Navigate to="/login" />
+                        )
+                    }
+                />
+            </Routes>
         </Router>
     );
 };
